d5074e83a3aed9e0e584d7f702e37c863a8e8c9a
AJ Schrier <ajschrier@gmail.com>
2016-08-02 00:16:47 -0700

    Changes to workingfile

--- ProblemSet3-1.py
+++ ProblemSet3-1.py
@@ -4,20 +4,6 @@ def f(x):
 
 
 def radiationExposure(start, stop, step):
-    '''
-    Computes and returns the amount of radiation exposed
-    to between the start and stop times. Calls the 
-    function f (defined for you in the grading script)
-    to obtain the value of the function at any point.
- 
-    start: integer, the time at which exposure begins
-    stop: integer, the time at which exposure ends
-    step: float, the width of each rectangle. You can assume that
-      the step size will always partition the space evenly.
-
-    returns: float, the amount of radiation exposed to 
-      between start and stop times.
-    '''
     i = start
     customRange = []
     while i < stop:
@@ -29,11 +15,7 @@ def radiationExposure(start, stop, step):
     return r
 
 
-print radiationExposure(0,5,1) - 39.10318784326239
-print radiationExposure(5,11,1) - 22.94241041057671
-print radiationExposure(0,11,1) - 62.0455982538
-print radiationExposure(40,100,1.5) - 0.434612356115
-# print 
-
-# print f(5)
-# print f(0)
+print radiationExposure(0, 5, 1) - 39.10318784326239
+print radiationExposure(5, 11, 1) - 22.94241041057671
+print radiationExposure(0, 11, 1) - 62.0455982538
+print radiationExposure(40, 100, 1.5) - 0.434612356115
